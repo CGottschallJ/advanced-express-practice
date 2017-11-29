e435872a214be73e827d90e937620d7bd75cb9e9
CGottschallJ <Cameron.Gottschall@gmail.com>
2017-11-29 15:39:24 -0600

    Initial Commit:

--- server/index.js
+++ server/index.js
@@ -3,8 +3,45 @@ import comments from "./comments";
 import products from "./products";
 import vehicles from "./vehicles";
 import contacts from "./contacts";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
+
+//
+// /* Getting Contacts */
+//
+// //list
+app.get("/contacts", (request, response) => {
+   response.json();
+   //request.body
+})
+//
+// //Individual contacts
+//
+//
+// /* Getting Comments */
+// //listen
+//
+//
+// //Individual comments
+//
+//
+// /* Getting Vehicles */
+// //list
+//
+//
+// //Indidual vehicles
+//
+//
+// /* Getting Products */
+// // listen
+//
+//
+// //Individual product
+
+
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
