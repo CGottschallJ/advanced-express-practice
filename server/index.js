12985835a4dc0da19773c5cf3d1564aca6d1f090
CGottschallJ <Cameron.Gottschall@gmail.com>
2018-01-15 17:03:48 -0600

    Second:  Through creating routes.  Todo: Create Route Files

--- server/index.js
+++ server/index.js
@@ -9,82 +9,84 @@ const app = express();
 app.use(bodyParser.json());
 
 
-/* Express Route for Contacts */
+///////* Express Route for Contacts */
 
 //All Contacts
 app.get("/contacts", (request, response) => {
   response.json(contacts);
+  //console.log('Contacts', contacts);
 });
 
 //Single Contact
-app.get("/contacts"/:id, (request, response) => {
-  response.json(contacts.id);
+app.get("/contacts/:id", (request, response) => {
+  let id = request.params.id;
+  console.log(id);
+  response.json(contacts.find(contact => contact._id == id));
 });
 
+//Create Contact
+app.post("/contacts", (request, response) => {
+  response.json(contacts.push(request.body));
+})
 
-
-/* Express Route for Vehicles */
+///////* Express Route for Vehicles */
 
 //All Vehicles
 app.get("/vehicles", (request, response) => {
   response.json(vehicles);
+  //console.log('Vehicles', vehicles);
 });
 
 //Single Vehicle
+app.get("/vehicles/:id", (request, response) => {
+  let id = request.params.id;
+  response.json(vehicles.find(vehicle => vehicle._id == id));
+})
 
+//Create Vehicle
+app.post("/vehicles", (request, response) => {
+  response.json(vehicles.push(request.body));
+})
 
-/* Express Route for Comments */
+///////* Express Route for Comments */
 
 //All Comments
 app.get("/comments", (request, response) => {
   response.json(comments);
+  //console.log('Comments', comments);
 });
 
 //Single Comment
+app.get("/comments/:id", (request,response) => {
+  let id= request.params.id;
+  response.json(comments.find(comment => comment._id == id));
+})
 
+//Create Comment
+app.post("/comments", (request, response) => {
+  response.json(comments.push(request.body));
+})
 
-/* Express Route for Products */
+///////* Express Route for Products */
 
 //All Products
 app.get("/products", (request, response) => {
   response.json(products);
+  //console.log('Products', products);
 });
 
 //Single Product
+app.get("/products/:id", (request, response) => {
+  let id = request.params.id;
+  response.json(products.find(product => product._id == id));
+});
 
-/* Getting Contacts */
-// //list
-// app.get("/contacts", (request, response) => {
-//    response.json();
-//    //request.body
-// })
-//
-// //Individual contacts
-//
-//
-// /* Getting Comments */
-// //listen
-//
-//
-// //Individual comments
-//
-//
-// /* Getting Vehicles */
-// //list
-//
-//
-// //Indidual vehicles
-//
-//
-// /* Getting Products */
-// // listen
-//
-//
-// //Individual product
+//Create Product
+app.post("/products", (request, response) => {
+  response.json(products.push(request.body));
+})
 
-
-
-
+//////
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Listening on port:${port}`);
