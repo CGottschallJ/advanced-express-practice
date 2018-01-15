c265eb270c2352130d0b9dc37373ee1dee6d9659
CGottschallJ <Cameron.Gottschall@gmail.com>
2018-01-14 18:52:49 -0600

    Initial: Complete through ###Create Express Routes to get all things. Begun: ###Get one thing

--- server/index.js
+++ server/index.js
@@ -8,14 +8,56 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
-//
-// /* Getting Contacts */
-//
-// //list
+
+/* Express Route for Contacts */
+
+//All Contacts
 app.get("/contacts", (request, response) => {
-   response.json();
-   //request.body
-})
+  response.json(contacts);
+});
+
+//Single Contact
+app.get("/contacts"/:id, (request, response) => {
+  response.json(contacts.id);
+});
+
+
+
+/* Express Route for Vehicles */
+
+//All Vehicles
+app.get("/vehicles", (request, response) => {
+  response.json(vehicles);
+});
+
+//Single Vehicle
+
+
+/* Express Route for Comments */
+
+//All Comments
+app.get("/comments", (request, response) => {
+  response.json(comments);
+});
+
+//Single Comment
+
+
+/* Express Route for Products */
+
+//All Products
+app.get("/products", (request, response) => {
+  response.json(products);
+});
+
+//Single Product
+
+/* Getting Contacts */
+// //list
+// app.get("/contacts", (request, response) => {
+//    response.json();
+//    //request.body
+// })
 //
 // //Individual contacts
 //
